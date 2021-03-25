5c9fcafa98a31f83472537bfd9b6cb04a4bc3bb2
alexandre-aco-code <alexandre.connanglecode@gmail.com>
2021-03-25 15:40:32 +0100

    autoload added on other files

--- 2-panier/confirm.php
+++ 2-panier/confirm.php
@@ -1,21 +1,5 @@
 <?php
-		include 'interfaces/IConnect.php';
-	include 'interfaces/IStorage.php';
-	include 'classes/Product.php';
-	include 'classes/ProductList.php';
-	include 'classes/ProductOrder.php';
-	include 'classes/Cart.php';
-	include 'classes/Session.php';
-	include 'classes/Database.php';
-
-	include 'interfaces/IProductLoader.php';
-	include 'classes/DatabaseProductList.php';
-	include 'classes/MemoryProductList.php';
-
-	include 'interfaces/IShipping.php';
-	include 'classes/Shipping.php';
-	include 'classes/ShippingConfinement.php';
-	include 'classes/ShippingNoel.php';
+	include 'includes/autoload.php';	
 
 
 	
