9f05512e24d07679ec5889bc685366512109b328
alexandre-aco-code <alexandre.connanglecode@gmail.com>
2021-03-26 12:38:46 +0100

    putting fun examples and still working

--- 5-starbuck/index.php
+++ 5-starbuck/index.php
@@ -3,8 +3,6 @@
 include 'includes/autoload.php';
 
 $EspressoMedium = new Drink('Espresso',1.20, 'medium');
-// $Chocolat = new Drink('Chocolat',2.10, 'medium');
-// $The = new Drink('Thé',1.70, 'large');
 
 echo "<br>" . $EspressoMedium->getTotalPrice();
 echo "<br>" . $EspressoMedium->getDescription();
@@ -23,4 +21,26 @@ $EspressoMediumWithCreamWithChocolate = new Chocolate($EspressoMediumWithCream);
 
 echo "<br>" . $EspressoMediumWithCreamWithChocolate->getTotalPrice();
 echo "<br>" . $EspressoMediumWithCreamWithChocolate->getDescription();
+
+
+$TheLarge = new Drink('Thé',1.70, 'large');
+echo "<br>" . $TheLarge->getTotalPrice();
+echo "<br>" . $TheLarge->getDescription();
+
+$TheLargeWithCream = new Cream($TheLarge);
+echo "<br>" . $TheLargeWithCream->getTotalPrice();
+echo "<br>" . $TheLargeWithCream->getDescription();
+
+
+$Chocolat = new Drink('Chocolat',2.10, 'small');
+echo "<br>" . $Chocolat->getTotalPrice();
+echo "<br>" . $Chocolat->getDescription();
+
+$ChocolatWithCreamWithMilkWithChantillyWithChocolat = new Chocolate ( new Chantilly ( new Milk ( new Cream ( $Chocolat ))));
+echo "<br>" . $ChocolatWithCreamWithMilkWithChantillyWithChocolat->getTotalPrice();
+echo "<br>" . $ChocolatWithCreamWithMilkWithChantillyWithChocolat->getDescription();
+
+
+
+
 
